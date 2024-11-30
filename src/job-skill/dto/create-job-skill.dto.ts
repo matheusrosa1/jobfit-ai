daf6b8c0ea435ef60cea78957c185155d5399984
Matheus Rosa <matheusrosataxa@gmail.com>
2024-11-30 16:24:40 -0300

    fix: create job skill

--- src/job-skill/dto/create-job-skill.dto.ts
+++ src/job-skill/dto/create-job-skill.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, IsInt, Min } from 'class-validator';
+import { IsNotEmpty, IsInt, Min, IsUUID } from 'class-validator';
 
 export class CreateJobSkillDto {
   @ApiProperty({
     description: 'The ID of the job.',
     example: 'fcf7c8da-9262-4957-a416-0062d1c1a07e',
   })
-  @IsString()
+  @IsUUID()
   @IsNotEmpty()
   jobId: string;
 
@@ -14,7 +14,7 @@ export class CreateJobSkillDto {
     description: 'The ID of the skill.',
     example: 'db578fe0-83d3-47c8-835b-d8d900ee45c3',
   })
-  @IsString()
+  @IsUUID()
   @IsNotEmpty()
   skillId: string;
 
