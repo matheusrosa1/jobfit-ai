58707fb0ea7698e73f401098202d25dde46997d5
Matheus Rosa <matheusrosataxa@gmail.com>
2024-12-03 22:15:46 -0300

    fix: update job skill e user skill dtos

--- src/job-skill/dto/update-job-skill.dto.ts
+++ src/job-skill/dto/update-job-skill.dto.ts
@@ -2,28 +2,12 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateJobSkillDto } from './create-job-skill.dto';
 import { IsInt, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { Exclude } from 'class-transformer';
 
 export class UpdateJobSkillDto extends PartialType(CreateJobSkillDto) {
-  @Exclude()
-  @ApiProperty({
-    description: 'The ID of the job (read-only). It cannot be updated.',
-    example: '295831db-5d2b-4e61-a108-cb362ab1d36d',
-    readOnly: true,
-  })
-  jobId: string;
-
-  @Exclude()
-  @ApiProperty({
-    description: 'The ID of the skill (read-only). It cannot be updated.',
-    example: '641ee8ee-9f27-4412-8870-6fc1770f08b9',
-    readOnly: true,
-  })
-  skillId: string;
-
   @ApiProperty({
     description: 'Years of experience required for the skill.',
     example: 3,
+    required: true,
   })
   @IsInt()
   @Min(0)
--- src/user-skill/dto/update-user-skill.dto.ts
+++ src/user-skill/dto/update-user-skill.dto.ts
@@ -1,27 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserSkillDto } from './create-user-skill.dto';
-import { IsInt, IsNotEmpty, IsUUID, Min } from 'class-validator';
+import { IsInt, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserSkillDto extends PartialType(CreateUserSkillDto) {
-  @ApiProperty({
-    description: 'The ID of the user.',
-    example: '1',
-    required: true,
-  })
-  @IsUUID()
-  @IsNotEmpty()
-  userId: string;
-
-  @ApiProperty({
-    description: 'The ID of the skill.',
-    example: '1',
-    required: true,
-  })
-  @IsUUID()
-  @IsNotEmpty()
-  skillId: string;
-
   @ApiProperty({
     description: 'The years of experience the user has with the skill.',
     example: 5,
