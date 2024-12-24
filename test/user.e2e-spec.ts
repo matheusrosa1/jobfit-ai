ad4df7109395492c7d81445f3bd892f6ae893bfb
matheusrosa1 <matheusrosataxa@gmail.com>
2024-12-24 09:50:07 -0300

    feat: testes user funcionando

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,20 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
-
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
 import * as request from 'supertest'; // Usando supertest para testar a API
 import { INestApplication } from '@nestjs/common';
 import { UserService } from '../src/user/user.service';
 import { User } from '../src/user/entities/user.entity';
 import { UserController } from '../src/user/user.controller';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
-import { JwtStrategy } from '../src/auth/jwt.strategy';
 
 describe('UserController (Integration)', () => {
-  let app: INestApplication; // Alterando para usar a aplicação como parâmetro
+  let app: INestApplication;
   let service: UserService;
   let repository: Repository<User>;
+  let jwtService: JwtService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -33,25 +32,27 @@ describe('UserController (Integration)', () => {
           },
         },
         {
-          provide: JwtStrategy, // Mockando a estratégia JWT
+          provide: JwtService,
           useValue: {
-            validate: jest.fn().mockResolvedValue({ id: 'some-uuid', email: 'test@example.com' }), // Mocka o método de validação
+            signAsync: jest.fn(() => Promise.resolve('fake-jwt-token')), // Mock do método signAsync
+            verifyAsync: jest.fn(() => Promise.resolve({ sub: 'user-id', email: 'test@example.com' })), // Mock do método verifyAsync
           },
         },
-        {
-          provide: AuthGuard('jwt'), // Mockando o AuthGuard
-          useValue: {
-            canActivate: jest.fn(() => true), // Sempre retorna true, como se o JWT fosse válido
-          },
-        },
+        
       ],
-    }).compile();
+    })  
+    .overrideGuard(AuthGuard('jwt'))
+    .useValue({
+      canActivate: jest.fn(() => true), // Sempre permitir acesso
+    })
+    .compile();
 
     app = module.createNestApplication();
     await app.init();
 
     service = module.get<UserService>(UserService);
     repository = module.get<Repository<User>>(getRepositoryToken(User));
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('deve criar um usuário', async () => {
@@ -64,25 +65,30 @@ describe('UserController (Integration)', () => {
 
     jest.spyOn(service, 'create').mockResolvedValue(createUserDto as any); // Mock do service
 
-    return request(app.getHttpServer()) // Usando a instância da aplicação aqui
+    return request(app.getHttpServer())
       .post('/users')
       .send(createUserDto)
       .expect(201)
       .expect((res) => {
         expect(res.body).toEqual({
           ...createUserDto,
-          password: expect.any(String), // Verifica que a senha está sendo retornada, mas não é a original
         });
       });
   });
 
   it('deve remover um usuário', async () => {
     const id = 'some-uuid';
+
+    jest.spyOn(jwtService, 'verifyAsync').mockResolvedValueOnce({
+      sub: 'some-user-id',
+      email: 'test@example.com',
+    }); // Mock do JWT verification
+
     jest.spyOn(service, 'remove').mockResolvedValueOnce({ affected: 1 } as any);
 
-    return request(app.getHttpServer()) // Usando a instância da aplicação aqui
+    return request(app.getHttpServer())
       .delete(`/users/${id}`)
-      .set('Authorization', 'Bearer fake-jwt-token') // Passando um token JWT falso
+      .set('Authorization', 'Bearer fake-jwt-token') // Passando o token JWT mockado
       .expect(200)
       .expect((res) => {
         expect(res.body).toEqual({ affected: 1 });
